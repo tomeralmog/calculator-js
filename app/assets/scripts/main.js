182806367e03d2449f242e7d48c5c9da38dbbbf0
talmog@spokeo.com <talmog@spokeo.com>
2016-11-11 21:34:55 -0800

    make methods private

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -34,127 +34,178 @@
     this.bindEvents();
   }
 
-  Calculator.prototype.reset = function () {
-    this.result = 0;
-    this.textResult = '';
-    this.operation = '';
-    this.screen.value = '0';
-  };
+  Calculator.prototype = {
+    constructor : Calculator,
 
-  Calculator.prototype.bindEvents = function () {
-    bindEventListener(this.buttons, 'click', bind(this, this.onButtonPressed));
-    bindEventListener(window, 'keypress', bind(this, this.onKeyPressed));
-  };
-
-  Calculator.prototype.onButtonPressed = function (e) {
-    if(!e.srcElement || this.blockActions){
-      return;
-    }
-
-    this.blockActions = true;
-    var numCharacter = e.srcElement.innerText;
-    if(numCharacter.length > 1){
-      this.blockActions = false;
-      return;
-    }
-
-    if(!isNaN(parseInt(numCharacter)) || numCharacter === '.'){
-      this.inputNumber(numCharacter);
-    } else {
-      console.log(numCharacter, numCharacter.length);
-
-      if(numCharacter.length === 1) {
-        this.inputOperator(numCharacter);
-      }
-    }
-    this.blockActions = false;
-
-  };
-
-  Calculator.prototype.onKeyPressed = function (e) {
-    console.log(e)
-  };
-
-  Calculator.prototype.inputNumber = function (numCharacter) {
-    if( this.textResult.slice(-1) === '.' && numCharacter=== '.'){
-      return;
-    }
-    if(this.operation === '') {
-      this.textResult += numCharacter;
-      if (this.textResult === '.'){ this.textResult = '0.'}
-      this.result = parseFloat(this.textResult);
-      this.screen.value = this.textResult;
-    } else {
-      this.textResult += numCharacter;
-      this.screen.value = this.textResult;
-    }
-  };
-
-  Calculator.prototype.inputOperator = function (operator) {
-    if (operator === 'C') {
-      this.reset();
-    } else {
-      if(this.operation === '') {
-        this.operation = operator;
-        this.textResult = '';
-      } else if(this.textResult !== '' ){
-        this.calculatePrevOperation(operator);
-      }
-    }
-  };
-
-  Calculator.prototype.calculatePrevOperation = function (nextOperator) {
-
-    switch (this.operation) {
-      case '+':
-        this.result += parseFloat(this.textResult);
-        break;
-      case '-':
-        this.result -= parseFloat(this.textResult);
-        break;
-      case 'x':
-        this.result *= parseFloat(this.textResult);
-        break;
-      case '^':
-        this.result = Math.pow(this.result, parseFloat(this.textResult));
-        break;
-      case '÷':
-        if (parseFloat(this.textResult) === 0) {
-          this.showError('Error: Can\'t divide by 0');
-          return;
-        } else {
-          this.result /= parseFloat(this.textResult);
-          break;
-        }
-
-
-    }
-
-    this.textResult = '';
-    console.log('here?', this.operation);
-    this.screen.value = this.result;
-    if (nextOperator === '=') {
+    reset : function () {
+      this.result = 0;
+      this.textResult = '';
       this.operation = '';
-    } else {
-      this.operation = nextOperator;
+      this.screen.value = '0';
+    },
+
+    bindEvents : function () {
+      bindEventListener(this.buttons, 'click', bind(this, this.onButtonPressed));
+      bindEventListener(window, 'keypress', bind(this, this.onKeyPressed));
+    },
+
+    onButtonPressed : function (e) {
+      if(!e.srcElement || this.blockActions){
+        return;
+      }
+
+      this.blockActions = true;
+      var numCharacter = e.srcElement.innerText;
+      if(numCharacter.length > 1){
+        this.blockActions = false;
+        return;
+      }
+
+      if(!isNaN(parseInt(numCharacter)) || numCharacter === '.'){
+        this.inputNumber(numCharacter);
+      } else {
+        console.log(numCharacter, numCharacter.length);
+
+        if(numCharacter.length === 1) {
+          this.inputOperator(numCharacter);
+        }
+      }
+      this.blockActions = false;
+
+    },
+
+    onButtonPressed : function (e) {
+      if(!e.srcElement || this.blockActions){
+        return;
+      }
+
+      this.blockActions = true;
+      var numCharacter = e.srcElement.innerText;
+      if(numCharacter.length > 1){
+        this.blockActions = false;
+        return;
+      }
+
+      if(!isNaN(parseInt(numCharacter)) || numCharacter === '.'){
+        this.inputNumber(numCharacter);
+      } else {
+        console.log(numCharacter, numCharacter.length);
+
+        if(numCharacter.length === 1) {
+          this.inputOperator(numCharacter);
+        }
+      }
+      this.blockActions = false;
+
+    },
+
+    onKeyPressed : function (e) {
+      console.log(e)
+    },
+
+    inputNumber : function (numCharacter) {
+      if( this.textResult.slice(-1) === '.' && numCharacter=== '.'){
+        return;
+      }
+      if(this.operation === '') {
+        this.textResult += numCharacter;
+        if (this.textResult === '.'){ this.textResult = '0.'}
+        this.result = parseFloat(this.textResult);
+        this.screen.value = this.textResult;
+      } else {
+        this.textResult += numCharacter;
+        this.screen.value = this.textResult;
+      }
+    },
+
+    inputOperator : function (operator) {
+      if (operator === 'C') {
+        this.reset();
+      } else {
+        if(this.operation === '') {
+          this.operation = operator;
+          this.textResult = '';
+        } else if(this.textResult !== '' ){
+          this.calculatePrevOperation(operator);
+        }
+      }
+    },
+
+    inputNumber : function (numCharacter) {
+      if( this.textResult.slice(-1) === '.' && numCharacter=== '.'){
+        return;
+      }
+      if(this.operation === '') {
+        this.textResult += numCharacter;
+        if (this.textResult === '.'){ this.textResult = '0.'}
+        this.result = parseFloat(this.textResult);
+        this.screen.value = this.textResult;
+      } else {
+        this.textResult += numCharacter;
+        this.screen.value = this.textResult;
+      }
+    },
+
+    inputOperator : function (operator) {
+      if (operator === 'C') {
+        this.reset();
+      } else {
+        if(this.operation === '') {
+          this.operation = operator;
+          this.textResult = '';
+        } else if(this.textResult !== '' ){
+          this.calculatePrevOperation(operator);
+        }
+      }
+    },
+
+    calculatePrevOperation : function (nextOperator) {
+      switch (this.operation) {
+        case '+':
+          this.result += parseFloat(this.textResult);
+          break;
+        case '-':
+          this.result -= parseFloat(this.textResult);
+          break;
+        case 'x':
+          this.result *= parseFloat(this.textResult);
+          break;
+        case '^':
+          this.result = Math.pow(this.result, parseFloat(this.textResult));
+          break;
+        case '÷':
+          if (parseFloat(this.textResult) === 0) {
+            this.showError('Error: Can\'t divide by 0');
+            return;
+          } else {
+            this.result /= parseFloat(this.textResult);
+            break;
+          }
+      }
+
+      this.textResult = '';
+      console.log('here?', this.operation);
+      this.screen.value = this.result;
+      if (nextOperator === '=') {
+        this.operation = '';
+      } else {
+        this.operation = nextOperator;
+      }
+    },
+
+    showError : function(errorMsg) {
+      this.screen.value = errorMsg;
+      this.screen.className = 'error';
+      this.blockActions = true;
+      var self = this;
+      setTimeout(function(){
+        self.reset();
+        self.screen.className = '';
+        self.blockActions = false;
+      }, 2000);
     }
 
   };
-
-  Calculator.prototype.showError = function (errorMsg) {
-    this.screen.value = errorMsg;
-    this.screen.className = 'error';
-    this.blockActions = true;
-    var self = this;
-    setTimeout(function(){
-      self.reset();
-      self.screen.className = '';
-      self.blockActions = false;
-    }, 2000);
-  };
-
-
-
 
   var calculator = new Calculator(s.screen, s.buttons);
 })();
